415b4ff53dd3da4e1cabddd7dc1e255221344afc
Cathal Reddin <cathal.reddin@bt.com>
2021-03-12 23:34:57 +0000

    Add 4 days to schedule

--- MealPlannerMVC/Controllers/HomeController.cs
+++ MealPlannerMVC/Controllers/HomeController.cs
@@ -90,6 +90,153 @@ namespace MealPlannerMVC.Controllers
 			{
 				mealPlan.Add(model.Meal6, model.Meal6B);
 			}
+			if (mealPlan.ContainsKey(model.Meal7))
+			{
+				mealPlan[model.Meal7] += model.Meal7B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal7, model.Meal7B);
+			}
+
+			if (mealPlan.ContainsKey(model.Meal8))
+			{
+				mealPlan[model.Meal8] += model.Meal8B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal8, model.Meal8B);
+			}
+			if (mealPlan.ContainsKey(model.Meal9))
+			{
+				mealPlan[model.Meal9] += model.Meal9B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal9, model.Meal9B);
+			}
+			if (mealPlan.ContainsKey(model.Meal10))
+			{
+				mealPlan[model.Meal10] += model.Meal10B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal10, model.Meal10B);
+			}
+			if (mealPlan.ContainsKey(model.Meal11))
+			{
+				mealPlan[model.Meal11] += model.Meal11B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal11, model.Meal11B);
+			}
+			if (mealPlan.ContainsKey(model.Meal12))
+			{
+				mealPlan[model.Meal12] += model.Meal12B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal12, model.Meal12B);
+			}
+			if (mealPlan.ContainsKey(model.Meal13))
+			{
+				mealPlan[model.Meal13] += model.Meal13B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal13, model.Meal13B);
+			}
+			if (mealPlan.ContainsKey(model.Meal14))
+			{
+				mealPlan[model.Meal14] += model.Meal14B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal14, model.Meal14B);
+			}
+			if (mealPlan.ContainsKey(model.Meal15))
+			{
+				mealPlan[model.Meal15] += model.Meal15B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal15, model.Meal15B);
+			}
+			if (mealPlan.ContainsKey(model.Meal16))
+			{
+				mealPlan[model.Meal16] += model.Meal16B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal16, model.Meal16B);
+			}
+			if (mealPlan.ContainsKey(model.Meal17))
+			{
+				mealPlan[model.Meal17] += model.Meal17B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal17, model.Meal17B);
+			}
+
+			if (mealPlan.ContainsKey(model.Meal18))
+			{
+				mealPlan[model.Meal18] += model.Meal18B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal18, model.Meal18B);
+			}
+			if (mealPlan.ContainsKey(model.Meal19))
+			{
+				mealPlan[model.Meal19] += model.Meal19B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal19, model.Meal19B);
+			}
+			if (mealPlan.ContainsKey(model.Meal20))
+			{
+				mealPlan[model.Meal20] += model.Meal20B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal20, model.Meal20B);
+			}
+			if (mealPlan.ContainsKey(model.Meal21))
+			{
+				mealPlan[model.Meal21] += model.Meal21B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal21, model.Meal21B);
+			}
+			if (mealPlan.ContainsKey(model.Meal22))
+			{
+				mealPlan[model.Meal22] += model.Meal22B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal22, model.Meal22B);
+			}
+
+			if (mealPlan.ContainsKey(model.Meal23))
+			{
+				mealPlan[model.Meal23] += model.Meal23B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal23, model.Meal23B);
+			}
+			if (mealPlan.ContainsKey(model.Meal24))
+			{
+				mealPlan[model.Meal24] += model.Meal24B;
+			}
+			else
+			{
+				mealPlan.Add(model.Meal24, model.Meal24B);
+			}
 			// have <MealID, Quantity>
 			mealPlan.Remove(0);
 			var ingredientPlan = new Dictionary<int, double>();
--- MealPlannerMVC/ViewModels/PlanViewModel.cs
+++ MealPlannerMVC/ViewModels/PlanViewModel.cs
@@ -26,6 +26,47 @@ namespace MealPlannerMVC.ViewModels
 		public int Meal5B { get; set; }
 		public int Meal6 { get; set; }
 		public int Meal6B { get; set; }
+		public int Meal7 { get; set; }
+		public int Meal7B { get; set; }
+		public int Meal8 { get; set; }
+		public int Meal8B { get; set; }
+
+		public int Meal9 { get; set; }
+		public int Meal9B { get; set; }
+		public int Meal10 { get; set; }
+		public int Meal10B { get; set; }
+
+		public int Meal11 { get; set; }
+		public int Meal11B { get; set; }
+		public int Meal12 { get; set; }
+		public int Meal12B { get; set; }
+		public int Meal13 { get; set; }
+		public int Meal13B { get; set; }
+		public int Meal14 { get; set; }
+		public int Meal14B { get; set; }
+
+		public int Meal15 { get; set; }
+		public int Meal15B { get; set; }
+		public int Meal16 { get; set; }
+		public int Meal16B { get; set; }
+		public int Meal17 { get; set; }
+		public int Meal17B { get; set; }
+		public int Meal18 { get; set; }
+		public int Meal18B { get; set; }
+
+		public int Meal19 { get; set; }
+		public int Meal19B { get; set; }
+		public int Meal20 { get; set; }
+		public int Meal20B { get; set; }
+
+		public int Meal21 { get; set; }
+		public int Meal21B { get; set; }
+		public int Meal22 { get; set; }
+		public int Meal22B { get; set; }
+		public int Meal23 { get; set; }
+		public int Meal23B { get; set; }
+		public int Meal24 { get; set; }
+		public int Meal24B { get; set; }
 		public IEnumerable<Meal> Meals { get; set; }
 		public List<Ingredient> ShoppingList { get; set; }
 	}
